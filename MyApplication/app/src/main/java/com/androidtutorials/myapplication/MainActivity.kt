2f609576af35172095a830afd2a3a887db5342e4
VinayTShetty <vinaytshetty@gmail.com>
2024-11-23 18:42:42 +0530

    StateManagement implementation on remember mutableStateOf.

--- MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
@@ -1,46 +1,70 @@
 package com.androidtutorials.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.androidtutorials.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "STATE_MANAGEMENT"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            mainScreen()
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun mainScreen() {
+
+        Log.i(TAG, "mainScreen_Composoable executed")
+
+        var counter = remember{ mutableStateOf(0) }
+
+        var incerementCounter = {
+            counter.value=++counter.value
+            Log.i(TAG, "IncrementLogic: ${counter.value.toString()}")
+        }
+
+        var decrementCounter = {
+            counter.value=--counter.value
+            Log.i(TAG, "DecrementLogic: ${counter.value.toString()}")
+        }
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = {
+                    incerementCounter.invoke()
+                    Log.i(TAG, "Increment Counter clicked ")
+                }) {
+                    Text(text = "Incerement")
+                }
+                Text("${counter.value.toString()}")
+                Button(onClick = {
+                    decrementCounter.invoke()
+                    Log.i(TAG, "DecrementCounter")
+                }) {
+                    Text(text = "Decrement")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
