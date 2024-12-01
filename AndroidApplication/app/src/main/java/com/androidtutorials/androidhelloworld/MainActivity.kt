8850aecf57a7a429f773fd99883ee1654a761c67
VinayTShetty <vinaytshetty@gmail.com>
2024-12-01 15:59:06 +0530

    Data Binding example implementation.

--- AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
+++ AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.androidtutorials.androidhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+
+import com.androidtutorials.androidhelloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var user: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        user = User("John")
+        binding.btnClickme.setOnClickListener {
+            binding.userLayout= user
+        }
     }
 }
---
+++ AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/User.kt
@@ -0,0 +1,3 @@
+package com.androidtutorials.androidhelloworld
+
+data class User(val username:String)
