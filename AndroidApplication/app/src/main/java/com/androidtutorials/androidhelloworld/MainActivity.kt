cb5d44403bd6fa6e639ba6a70e88a6620cf1680e
VinayTShetty <vinaytshetty@gmail.com>
2024-12-01 09:09:15 +0530

    ViewBinding Basic Example

--- AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
+++ AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
@@ -2,10 +2,23 @@ package com.androidtutorials.androidhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.androidtutorials.androidhelloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //inflate the layout using view Binding
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textMessage.text="No Name"
+        binding.btnClickme.setOnClickListener {
+            binding.textMessage.text="Hello Vinay"
+            Toast.makeText(this,binding.textMessage.text,Toast.LENGTH_LONG).show()
+        }
     }
 }
