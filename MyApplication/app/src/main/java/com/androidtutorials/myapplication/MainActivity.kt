845a3b31dd7f25fedd3b556e452f73c8aac2845f
VinayTShetty <vinaytshetty@gmail.com>
2025-09-21 12:29:16 +0530

    Without StateManagement we can see that the UI is not responding to changes in the counter value.

--- MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
@@ -1,46 +1,78 @@
 package com.androidtutorials.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.androidtutorials.myapplication.MainActivity.Companion.TAG
 import com.androidtutorials.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MainScreen()
+            Log.d(TAG, "onCreate: ")
+        }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+
+    Log.d(TAG, " Inside MainScreen() method ")
+
+    var counter = 0
+
+    var incrementCounter = {
+        counter++
+        Log.d(TAG, " Counter Incremented $counter")
+    }
+
+    var decrementCounter = {
+        counter--
+        Log.d(TAG, " Counter Decremented $counter")
+    }
+
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = {
+                incrementCounter.invoke()
+            }) {
+                Text(text = "Increment")
+            }
+            Text(text = "$counter")
+
+            Button(onClick = {
+                decrementCounter.invoke()
+            }) {
+                Text(text = "Decrement")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    MainScreen()
 }
