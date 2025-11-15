7d05559b5efd0aa5186b73a10d5e45e3361c2321
VinayTShetty <vinaytshetty@gmail.com>
2025-11-15 11:47:00 +0530

    Implementation of produceState in stateManagement.
    Includes:
    - Collecting Flow using produceState
    - Auto lifecycle cancellation
    - Converting external async data sources into Compose State cleanly

--- MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
@@ -3,14 +3,25 @@ package com.androidtutorials.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.androidtutorials.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +33,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ProducedeStateFlowScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun numberFlow(): Flow<Int> =flow{
+    var counter=0
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+    while (true){
+        delay(1000)
+        emit(counter++)
     }
 }
+
+
+@Composable
+fun ProducedeStateFlowScreen(){
+
+    val count by produceState(0) {
+        numberFlow().collect { value   = it }
+    }
+
+    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "Live Counter From Flow", fontSize = 26.sp, color = MaterialTheme.colorScheme.primary)
+
+        Spacer(Modifier.height(20.dp))
+
+        Text(text = "Counter =$count" , fontSize = 30.sp, color = MaterialTheme.colorScheme.secondary)
+    }
+
+
+}
+
+
