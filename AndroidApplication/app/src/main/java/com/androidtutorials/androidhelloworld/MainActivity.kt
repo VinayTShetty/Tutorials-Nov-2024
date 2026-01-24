5cee835b83e0b18000b5fbb0969f798122874549
VinayTShetty <vinaytshetty@gmail.com>
2026-01-24 08:49:15 +0530

    Reverse of a String in Kotlin

--- AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
+++ AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("------------>"+reverseString("Interview"))
+    }
+
+
+    fun reverseString(input: String) : String{
+        var result=""
+
+        for(i in input.length-1 downTo 0){
+            result=result+input[i]
+        }
+        return result
     }
 }
