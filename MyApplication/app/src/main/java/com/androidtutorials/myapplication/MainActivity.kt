fc09390e2ce8e95645b5f3d79587989a0cd7233a
VinayTShetty <vinaytshetty@gmail.com>
2026-01-28 07:10:05 +0530

    FireBase Example. Basic FireBase Example of sending Data to Real Time DataBase.

--- MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/androidtutorials/myapplication/MainActivity.kt
@@ -3,44 +3,52 @@ package com.androidtutorials.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.androidtutorials.myapplication.ui.theme.MyApplicationTheme
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            sendDataUI()
+        }
+
+
+    }
+
+    private fun sendDataToFireBase() {
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("Users")
+
+        val user = mapOf(
+            "name" to "Vinay TS",
+            "age" to 28,
+            "City" to "Bangalore"
+        )
+
+        ref.push().setValue(user)
+    }
+
+    @Composable
+    fun sendDataUI() {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = {sendDataToFireBase()}) {
+                Text("Send Data")
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
 }
