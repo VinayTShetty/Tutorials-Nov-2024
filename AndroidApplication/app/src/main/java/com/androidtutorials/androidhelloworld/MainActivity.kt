d2b61daab1b8a1e79ee25fae03cc317c98a0e922
VinayTShetty <vinaytshetty@gmail.com>
2026-02-11 18:02:41 +0530

    Coroutine Builders runBlocking Return Generic Object. There is no Concept of Job/Await()

--- AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
+++ AndroidApplication/app/src/main/java/com/androidtutorials/androidhelloworld/MainActivity.kt
@@ -2,10 +2,31 @@ package com.androidtutorials.androidhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "RUN-BLOCKING"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ------------------------------------------------------------
+        // runBlocking is a coroutine builder
+        // It BLOCKS the current thread until coroutine finishes
+        // It RETURNS the last expression inside the block
+        // ------------------------------------------------------------
+
+        val result = runBlocking {
+
+            // This is the last expression
+            // So runBlocking will return 50
+            10 * 5
+        }
+
+        Log.d(TAG, "Result = $result")
     }
 }
